b7ef5b4814ed3af45f95b0a5affaa0674b4d478f
Asepsrn17 <asepsrn17@gmail.com>
2022-03-12 21:59:24 +0700

    revisi

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-import personData from "./table";
-personData();
-console.log(person);
+import personData from "./table.js";
+
+const person = new personData({
+    columns: ["Name", "Usia", "Email", "No. Telp"],
+    data: [
+        ["Asep", 30, "asepsrn17@gmail.com", "087722747"],
+        ["Bramanta", 30, "Braman14@gmail.com", "085859478"],
+        ["Baimas trisudi", 20, "baimas2.co.id", "0898737888"],
+        ["wahyu", 30, "wahyt24.co.id", "0857857488"],
+        ["Budi", 40, "buditr34@gmail.com", "082543468"]
+    ]
+});
+
+const app = document.getElementById("tableData");
+    person.render(app);
--- table.js
+++ table.js
@@ -41,19 +41,4 @@ class personData {
     }
 }
 
-const person = new personData({
-    columns: ["Name", "Usia", "Email", "No. Telp"],
-    data: [
-        ["Asep", 30, "asepsrn17@gmail.com", "087722747"],
-        ["Bramanta", 30, "Braman14@gmail.com", "085859478"],
-        ["Baimas trisudi", 20, "baimas2.co.id", "0898737888"],
-        ["wahyu", 30, "wahyt24.co.id", "0857857488"],
-        ["Budi", 40, "buditr34@gmail.com", "082543468"]
-    ]
-});
-
-const app = document.getElementById("tableData");
-person.render(app);
-
 export default personData;
-console.log(person);
